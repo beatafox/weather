79fbb0db88e7bf5b094cbf931c3ce80b93e7053b
beatafox <86629354+beatafox@users.noreply.github.com>
2021-06-29 21:20:36 +0200

    fetch weather data from api

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React, { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
-  const [lon, setLon] = useState("");
-  const [lat, setLat] = useState("");
+  const [long, setLon] = useState("");
+  const [latt, setLat] = useState("");
+  const [weather, setWeather] = useState("");
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
       setLon(position.coords.longitude);
       setLat(position.coords.latitude);
     });
-  }, []);
 
-  return (
-    <div className="App">
-      <span>Longitude is: {lon}</span>
-      <span>Latitude is: {lat}</span>
-    </div>
-  );
+    latt &&
+      long &&
+      fetch(
+        `https://www.metaweather.com/api/location/search/?lattlong=${latt},${long}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          fetch(`https://www.metaweather.com//api/location/${result[0].woeid}/`)
+            .then((res) => res.json())
+            .then((data) => {
+              setWeather(data);
+            });
+        });
+  }, [latt, long]);
+
+  return <div className="App"></div>;
 }
 
 export default App;
