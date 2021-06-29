5ea223e3b359ee2b309af14517ffaaf998cea39f
beatafox <86629354+beatafox@users.noreply.github.com>
2021-06-29 20:18:00 +0200

    get longitude and latitude from geolocation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [lon, setLon] = useState("");
+  const [lat, setLat] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLon(position.coords.longitude);
+      setLat(position.coords.latitude);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <span>Longitude is: {lon}</span>
+      <span>Latitude is: {lat}</span>
     </div>
   );
 }
