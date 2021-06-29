04f7c2d39e49b26a16f72b8569710835844398b1
beatafox <86629354+beatafox@users.noreply.github.com>
2021-06-29 22:22:36 +0200

    change title of the app

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -3,7 +3,7 @@ import "./loader.scss";
 const Loader = () => {
   return (
     <div className="loader-wrapper">
-      <div class="loader"></div>
+      <div className="loader"></div>
     </div>
   );
 };
