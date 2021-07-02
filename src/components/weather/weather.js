6957d7bb2a7723b78a8de1c4132f102e2a11a888
beatafox <86629354+beatafox@users.noreply.github.com>
2021-07-02 18:21:23 +0200

    display weather icon based on weather_state_abbr

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,14 +1,21 @@
-//import "./weather.scss";
+import "./weather.scss";
 import { formatNumber } from "./utils";
+import weatherIcon from "../weatherIcon/weatherIcon";
+
 const Weather = ({ weatherData }) => {
   console.log(weatherData);
+  const city = weatherData.parent.title;
+  const forecast = weatherData.consolidated_weather[0];
   return (
     <>
-      {weatherData.parent.title}
-      {weatherData.consolidated_weather[0].weather_state_name}
-      {formatNumber(weatherData.consolidated_weather[0].max_temp)}
-      {formatNumber(weatherData.consolidated_weather[0].min_temp)}
-      {formatNumber(weatherData.consolidated_weather[0].the_temp)}
+      <div className="weather-wrapper">
+        <div> {city}</div>
+        {weatherIcon(forecast.weather_state_abbr)}
+        <div> {forecast.weather_state_name}</div>
+        <div>{formatNumber(forecast.the_temp)}</div>
+        <div> Max: {formatNumber(forecast.max_temp)}</div>
+        <div>Min:{formatNumber(forecast.min_temp)}</div>
+      </div>
     </>
   );
 };
---
+++ src/components/weatherIcon/weatherIcon.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./weatherIcon.scss";
+import Clear from "../../assets/icons/clear.svg";
+import Hail from "../../assets/icons/hail.svg";
+import HeavyCloud from "../../assets/icons/heavy-cloud.svg";
+import HeavyRain from "../../assets/icons/heavy-rain.svg";
+import LightCloud from "../../assets/icons/light-cloud.svg";
+import LightRain from "../../assets/icons/light-rain.svg";
+import Showers from "../../assets/icons/showers.svg";
+import Sleet from "../../assets/icons/sleet.svg";
+import Snow from "../../assets/icons/snow.svg";
+import Thunderstorm from "../../assets/icons/thunderstorm.svg";
+
+function weatherIcon(weatherState) {
+  const key = weatherState?.toUpperCase();
+
+  let icon = (
+    <>
+      {
+        {
+          C: <img alt="" src={Clear}></img>,
+          H: <img alt="" src={Hail}></img>,
+          HC: <img alt="" src={HeavyCloud}></img>,
+          HR: <img alt="" src={HeavyRain}></img>,
+          LC: <img alt="" src={LightCloud}></img>,
+          LR: <img alt="" src={LightRain}></img>,
+          S: <img alt="" src={Showers}></img>,
+          SL: <img alt="" src={Sleet}></img>,
+          SN: <img alt="" src={Snow}></img>,
+          T: <img alt="" src={Thunderstorm}></img>,
+        }[key]
+      }
+    </>
+  );
+  return <div> {icon}</div>;
+}
+
+export default weatherIcon;
