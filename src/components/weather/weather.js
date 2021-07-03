1963613f372fefbfdeefc212c59fde9c390948f4
beatafox <86629354+beatafox@users.noreply.github.com>
2021-07-04 00:03:14 +0200

    add celsius and country to weather info

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -3,20 +3,20 @@ import { formatNumber } from "./utils";
 import weatherIcon from "../weatherIcon/weatherIcon";
 
 const Weather = ({ weatherData }) => {
-  console.log(weatherData);
-  const city = weatherData.parent.title;
   const forecast = weatherData.consolidated_weather[0];
   return (
     <>
       <div className="weather-wrapper">
-        <div> {city}</div>
+        <div className="weather-location">
+          {weatherData.title}, {weatherData.parent.title}
+        </div>
         <div className="weather-temp">
           {weatherIcon(forecast.weather_state_abbr)}
-          <div> {formatNumber(forecast.the_temp)}</div>
+          <div> {formatNumber(forecast.the_temp)}°C</div>
         </div>
         <div className="weather-name">{forecast.weather_state_name}</div>
-        <div> Max: {formatNumber(forecast.max_temp)}</div>
-        <div>Min: {formatNumber(forecast.min_temp)}</div>
+        <div> Max: {formatNumber(forecast.max_temp)}°C</div>
+        <div>Min: {formatNumber(forecast.min_temp)}°C</div>
       </div>
     </>
   );
