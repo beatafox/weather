5d7a89c69b456bee4d4666bb56880407e0451218
beatafox <86629354+beatafox@users.noreply.github.com>
2021-07-03 23:59:01 +0200

    fetch cities on input change and display mapped  results

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./search.scss";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      await fetch(
+        `https://www.metaweather.com/api/location/search/?query=${term}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setResults(data);
+        });
+    };
+    // condition for not searching empty string
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const searchResultsMap = results.map((result) => {
+    return (
+      <div className="search-item" key={result.title}>
+        <button>{result.title}</button>
+      </div>
+    );
+  });
 
   return (
     <div className="search-wrapper">
@@ -14,6 +39,7 @@ const Search = () => {
           onChange={(e) => setTerm(e.target.value)}
         />
       </div>
+      <div className="search-results">{searchResultsMap}</div>
     </div>
   );
 };
