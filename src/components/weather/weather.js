bc1d6017bce118ff870bf029294e65d06cae171b
beatafox <86629354+beatafox@users.noreply.github.com>
2021-07-04 02:23:26 +0200

    add map for weather data to display weather for six days

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -3,20 +3,26 @@ import weatherIcon from "../weatherIcon/weatherIcon";
 import "./weather.scss";
 
 const Weather = ({ weatherData }) => {
-  const forecast = weatherData.consolidated_weather[0];
   return (
     <>
       <div className="weather-wrapper">
         <div className="weather-location">
           {weatherData.title}, {weatherData.parent.title}
         </div>
-        <div className="weather-temp">
-          {weatherIcon(forecast.weather_state_abbr)}
-          <div>{formatNumber(forecast.the_temp)}°C</div>
-        </div>
-        <div className="weather-name">{forecast.weather_state_name}</div>
-        <div>Max: {formatNumber(forecast.max_temp)}°C</div>
-        <div>Min: {formatNumber(forecast.min_temp)}°C</div>
+        {weatherData.consolidated_weather.map((forecast) => {
+          return (
+            <div className="weather-card">
+              <div>{forecast.applicable_date}</div>
+              <div className="weather-temp">
+                {weatherIcon(forecast.weather_state_abbr)}
+                <div>{formatNumber(forecast.the_temp)}°C</div>
+              </div>
+              <div className="weather-name">{forecast.weather_state_name}</div>
+              <div>Max: {formatNumber(forecast.max_temp)}°C</div>
+              <div>Min: {formatNumber(forecast.min_temp)}°C</div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
